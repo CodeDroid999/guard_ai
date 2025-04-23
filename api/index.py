82090d38b7df54aabd93302f698d52b9456b0db4
CodeDroid999 <johnmwega2002@gmail.com>
2025-04-23 13:43:31 +0300

    update 1 file and delete 1 file

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
-from flask import Flask
+# app.py
+import cv2
+from flask import Flask, render_template, Response
+from ultralytics import YOLO
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+# Load model from model directory
+model_path = os.path.join('model', 'yolo11n.pt')
+model = YOLO(model_path)
 
-@app.route('/about')
-def about():
-    return 'About'
+# Capture from webcam
+cap = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+
+        results = model.predict(source=frame, show=False)
+
+        # Plot detection results on frame
+        for r in results:
+            annotated_frame = r.plot()
+
+        # Encode the frame for MJPEG
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
+        frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # HTML to show video stream
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
